3db08aedefc317c6429d2438b1423aab11a94ce6
Pearlkakande <158094230+Pearlkakande@users.noreply.github.com>
2024-01-29 23:08:40 +0300

    A quadratic root assignment in C

---
+++ main.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+// creating a function
+int func(int a,int b,int c){
+    int d=((b*b)-(4*a*c));
+    return d;
+}
+
+int main()
+{
+    int a,b,c;
+    printf("type a,b,and c \n");
+    scanf("%d , %d, %d", &a,&b,&c );
+
+    int discriminant = func(a,b,c);
+
+
+    if (discriminant>0) {
+        float x1, x2;
+        x1=((-b + sqrt(discriminant))/(2*a));
+        x2= ((-b - sqrt(discriminant))/(2*a));
+
+        printf("x1= %.2f \n x2= %.2f", x1,x2);
+    }
+
+    else if (discriminant==0){
+        float x= ((-b)/(2*a));
+        printf("the root is %f",x);
+
+    }
+    else{
+        printf("the root is complex");
+    }
+    return 0;
+}
